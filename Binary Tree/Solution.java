7d0ae7c728568d4617038320d6a461db38f577bb
Sri-Padmaraj22 <sripadmaraj2208@gmail.com>
2025-08-05 23:34:08 +0530

    Inorder Traversal done

---
+++ Binary Tree/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Solution
+{
+    public class TreeNode{
+        TreeNode left,right;
+        int val;
+
+        TreeNode(int data)
+        {
+            this.val=data;
+        }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private List<Integer> inorder(TreeNode node, List<Integer> result) {
+        if (node != null) {
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+        }
+        return result;
+    }
+}
