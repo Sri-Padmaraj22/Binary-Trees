5a01d4e3726eb209fe1bad80b2f260a576f51b43
Sri-Padmaraj22 <sripadmaraj2208@gmail.com>
2025-08-30 14:09:48 +0530

    Learnt Iterator in BST

---
+++ BinarySearchTree/BSTIterator.java
@@ -0,0 +1,37 @@
+package BinarySearchTree;
+
+import java.util.Stack;
+
+public class BSTIterator {
+    public class TreeNode{
+        TreeNode left,right;
+        int val;
+
+        TreeNode(int data)
+        {
+            this.val=data;
+        }
+    }
+    Stack<TreeNode> st=new Stack<TreeNode>();
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+    
+    public int next() {
+        TreeNode node=st.pop();
+        pushAll(node.right);
+        return node.val;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+    public void pushAll(TreeNode node)
+    {
+        while(node!=null)
+        {
+            st.push(node);
+            node=node.left;
+        }
+    }
+}
