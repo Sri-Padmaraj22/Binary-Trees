7373a58e00467a3952e82727347a49bbe9db12f1
Sri-Padmaraj22 <sripadmaraj2208@gmail.com>
2025-10-08 00:19:31 +0530

    Prime's Algorithm

---
+++ Graph/PrimsAlgorithm.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class PrimsAlgorithm {
+    class Pair{
+        int node,int distance;
+
+        Pair(int distance,int node)
+        {
+            this.node=node;
+            this.distance=distance;
+        }
+    }
+    
+    static int spanningTree(int V,ArrayList<ArrayList<ArrayList<Integer>>> adj)
+    {
+        int sum=0;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)-> x.distance-y.distance);
+        int vis[]=new int[V];
+        pq.add(new Pair(0,0));
+
+        while(!pq.isEmpty())
+        {
+            int node=pq.peek().node;
+            int wt=pq.peek().distance;
+            if(vis[node]==1)
+            continue;
+            sum+=wt;
+            vis[node]=1;
+            for(int i=0;i<adj.get(node).size();i++)
+            {
+                int adjwt=adj.get(node).get(i).get(1);
+                int adjnode=adj.get(node).get(i).get(0);
+                if(vis[adjnode]==0)
+                {
+                    pq.add(new Pair(adjwt,adjnode));
+                }
+            }
+        }
+        return sum;
+    }
+}
